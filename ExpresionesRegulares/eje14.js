3f333d6d95cdfd275756ea32ded6f13cefe96984
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-10-09 14:46:16 +0200

    Incompleto el ejercicio 14

--- ExpresionesRegulares/eje14.js
+++ ExpresionesRegulares/eje14.js
@@ -5,4 +5,9 @@
 var hora = '88:45';
 var regex = /^[0-9]{2}:[0-9]{2}$/;
 var ver = regex.test(hora);
+
+var horas = hora.substring(0,2);
+var minutos = hora.substring(2);
+console.log(`Las horas son: ${horas}`);
+console.log(`Los minutos son: ${minutos}`);
 console.log(ver);
