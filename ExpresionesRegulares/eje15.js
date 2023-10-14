4262f5fa262267816310416976a7bcc391a919a3
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-10-14 23:50:38 +0200

    EObject Math

--- ExpresionesRegulares/eje15.js
+++ ExpresionesRegulares/eje15.js
@@ -2,7 +2,13 @@
  * Los números pueden tener entre 1 y 3 dígitos. Solicita el ingreso de la cordenada de un punto. El formato a ingresar por teclado es (999,999)
  */
 
-var coordenada = '999,999';
+var coordenada = '2521,999';
 var regex = /^[0-9]{1,3},[0-9]{1,3}$/
-var comprobacion_coordebada = regex.test(coordenada);
-console.log(comprobacion_coordebada)
+//var comprobacion_coordenada = regex.test(coordenada);
+//console.log(comprobacion_coordenada)
+
+if(regex.test(coordenada)){
+    console.log("La coordenada es correcta.");
+}else{
+    console.log("La coordanda ingresada es incorrecta, utilice el formato (999,999).");
+}
---
+++ Math/eje1.js
@@ -0,0 +1,13 @@
+/**
+ * Hacer un programa que muestre un menú de opciones con las siguientes operaciones posibles:
+Redondeo a su inmediato superior de un número
+Redondeo a su inmediato inferior de un número
+Redondeo al número más cercano
+Cálculo del número máximo entre dos dados
+Cálculo del número mínimo entre dos dados
+Cálculo de un número elevado a otro
+Cálculo de la raíz cuadrada de un número
+
+ */
+
+var opcion = prompt("Indique la opcion: \n 1.Redondeo a su inmediato superior de un número \n 2.Redondeo a su inmediato inferior de un número \n 3.Redondeo al número más cercano \n 4.Cálculo del número máximo entre dos dados \n 5.Cálculo del número mínimo entre dos dados \n 6. Cálculo de un número elevado a otro \n 7.Cálculo de la raíz cuadrada de un número")
