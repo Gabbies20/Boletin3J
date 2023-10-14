96414c412bf4beae97e4e0554a13c5fd23c8c2f2
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-10-14 22:55:22 +0200

    Ejercciio15 hecho

---
+++ ExpresionesRegulares/eje15.js
@@ -0,0 +1,8 @@
+/**
+ * Los números pueden tener entre 1 y 3 dígitos. Solicita el ingreso de la cordenada de un punto. El formato a ingresar por teclado es (999,999)
+ */
+
+var coordenada = '999,999';
+var regex = /^[0-9]{1,3},[0-9]{1,3}$/
+var comprobacion_coordebada = regex.test(coordenada);
+console.log(comprobacion_coordebada)
