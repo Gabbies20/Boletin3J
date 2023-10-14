63d1a167a6b7e7d1f8147e3624cbab4c3deccdf6
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-10-14 23:59:34 +0200

    Object Math

--- Math/eje1.js
+++ Math/eje1.js
@@ -10,4 +10,46 @@ Cálculo de la raíz cuadrada de un número
 
  */
 
-var opcion = prompt("Indique la opcion: \n 1.Redondeo a su inmediato superior de un número \n 2.Redondeo a su inmediato inferior de un número \n 3.Redondeo al número más cercano \n 4.Cálculo del número máximo entre dos dados \n 5.Cálculo del número mínimo entre dos dados \n 6. Cálculo de un número elevado a otro \n 7.Cálculo de la raíz cuadrada de un número")
+var opcion = prompt("Indique la opcion: \n 1.Redondeo a su inmediato superior de un número \n 2.Redondeo a su inmediato inferior de un número \n 3.Redondeo al número más cercano \n 4.Cálculo del número máximo entre dos dados \n 5.Cálculo del número mínimo entre dos dados \n 6. Cálculo de un número elevado a otro \n 7.Cálculo de la raíz cuadrada de un número");
+var numero = 42.12;
+var numero2 = 15;
+switch (opcion) {
+    case '1':
+
+        console.log(Math.ceil(numero));
+        
+        break;
+    case '2':
+
+        console.log(Math.floor(numero));
+        
+        break;
+    case '3':
+
+        console.log(Math.round(numero));
+        
+        break;
+    case '4':
+
+        console.log(Math.max(numero,numero2));
+        
+        break;
+    case '5':
+
+        console.log(Math.ceil(numero));
+        
+        break;
+    case '6':
+
+        console.log(Math.ceil(numero));
+        
+        break;
+    case '7':
+
+        console.log(Math.ceil(numero));
+        
+        break;
+
+    default:
+        break;
+}
