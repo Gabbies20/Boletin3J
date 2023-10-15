5081d6b95b815151f69758a5a33a9a6bbd3324f7
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-10-15 23:17:23 +0200

    Object Date hasta Eje4.js

--- Object Date/eje3.js
+++ Object Date/eje3.js
@@ -1,17 +1,16 @@
 /**
  * Realiza un algoritmo que solicite del usuario un tiempo dado en segundos y calcule y presente en pantalla dicho tiempo pero expresado en horas, minutos y segundos.
- */
 
-// Solicitar al usuario un tiempo en segundos
-var tiempoEnSegundos = parseInt(prompt("Ingresa un tiempo en segundos:"));
+*/
 
-// Calcular horas, minutos y segundos
-var horas = Math.floor(tiempoEnSegundos / 3600);
-var minutos = Math.floor((tiempoEnSegundos % 3600) / 60);
-var segundos = tiempoEnSegundos % 60;
 
-// Presentar el resultado en pantalla
-console.log("El tiempo ingresado en horas, minutos y segundos es:");
-console.log("Horas: " + horas);
-console.log("Minutos: " + minutos);
-console.log("Segundos: " + segundos);
+var tiempo_segundos= 3665;
+
+var horas = Math.floor(tiempo_segundos / 3600);
+var minutos = Math.floor((tiempo_segundos % 3600)/60);
+var segundos = Math.floor(tiempo_segundos % 60)
+
+
+console.log(`La cantidad de horas es: ${horas}`);
+console.log(`La cantidad de minutos es: ${minutos}`);
+console.log(`La cantidad de segundos es: ${segundos}`);
--- Object Date/eje4.js
+++ Object Date/eje4.js
@@ -1,69 +1,30 @@
 /**
  * Calcular de un conjunto de fechas la menor, la mayor y la diferencia en segundos entre ambas.
  * 
- * function calcularFechas(fechas) {
-  if (fechas.length === 0) {
-    return "No hay fechas para calcular.";
-  }
-
-  // Encontrar la fecha menor
-  var fechaMenor = new Date(Math.min.apply(null, fechas));
-
-  // Encontrar la fecha mayor
-  var fechaMayor = new Date(Math.max.apply(null, fechas));
-
-  // Calcular la diferencia en segundos
-  var diferenciaEnSegundos = Math.abs((fechaMayor - fechaMenor) / 1000);
-
-  return {
-    fechaMenor: fechaMenor,
-    fechaMayor: fechaMayor,
-    diferenciaEnSegundos: diferenciaEnSegundos,
-  };
-}
-
-function calcularFechas(fechas) {
-  if (fechas.length === 0) {
-    return "No hay fechas para calcular.";
-  }
-
-  // Inicializar con la primera fecha del arreglo
-  var fechaMenor = fechas[0];
-  var fechaMayor = fechas[0];
-
-  // Recorrer el arreglo para encontrar la fecha menor y la fecha mayor
-  for (var i = 1; i < fechas.length; i++) {
-    if (fechas[i] < fechaMenor) {
-      fechaMenor = fechas[i];
-    }
-    if (fechas[i] > fechaMayor) {
-      fechaMayor = fechas[i];
-    }
-  }
-
-  // Calcular la diferencia en segundos
-  var diferenciaEnSegundos = Math.abs((fechaMayor - fechaMenor) / 1000);
-
-  return {
-    fechaMenor: fechaMenor,
-    fechaMayor: fechaMayor,
-    diferenciaEnSegundos: diferenciaEnSegundos,
-  };
-}
-
-// Ejemplo de uso
-var fechas = [
-  new Date('2023-01-15'),
-  new Date('2023-05-20'),
-  new Date('2023-02-10'),
-  new Date('2023-03-30'),
-];
-
-var resultados = calcularFechas(fechas);
-
-console.log("Fecha menor: " + resultados.fechaMenor);
-console.log("Fecha mayor: " + resultados.fechaMayor);
-console.log("Diferencia en segundos: " + resultados.diferenciaEnSegundos + " segundos");
-
-
  */
+
+var fecha1 = new Date("2001-01-02");
+var fecha2 = new Date("2015-02-06");
+var fecha3 = new Date("1990-05-02");
+var fechas = [fecha1.getTime(),fecha2.getTime(),fecha3.getTime()];
+
+var fecha_mayor = fechas[0];
+var fecha_menor = fechas[0];
+
+console.log(fechas);
+
+for (let index = 0; index < fechas.length; index++) {
+  const element = fechas[index];
+  console.log(element);
+  if(fechas[index] < fecha_menor){
+    fecha_menor = fechas[index];
+  }
+  if(fechas[index] > fecha_mayor){
+    fecha_mayor = fechas[index];
+  }
+
+  
+}
+
+console.log(fecha_menor);
+console.log(fecha_mayor);
