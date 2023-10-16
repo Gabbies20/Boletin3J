929b16c908423f6e437d32a001db287cc0f10cdf
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-10-16 11:56:01 +0200

    Hacer el ejercicio 4,5 y 6

--- Object Date/eje5.js
+++ Object Date/eje5.js
@@ -2,3 +2,10 @@
  * Realizar un programa que muestre cuántos días faltan para el próximo cumpleaños del usuario y muestre “¡Felicidades!” si es el día señalado.
  */
 
+
+var fecha_proximo_cumpleaños = new Date('2024-02-23');
+var fecha_actual = new Date();
+
+
+console.log(fecha_proximo_cumpleaños);
+console.log(fecha_actual);
